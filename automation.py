c1e629638b928a5ff6e308888082d143dda7027f
Adam Sadiq <adamsadiq@Adams-MacBook-Pro-4.local>
2019-08-23 10:24:50 +0100

    Documentation complete via comments

--- automation.py
+++ automation.py
@@ -6,12 +6,17 @@ import os
 import sys
 from datetime import date
 
-# import requests_cache ## pip install requests-cache --user
 from requests.auth import HTTPBasicAuth
+
+
+# Admin account should be used to be able to access all boards
+# Main problem to tackle is to figure out why this program returns more cards than leankit, changing the results
+# The cards all end before the end date, so there may be a bug from LeanKit, or in the API.
 
 # Retrieve account name from user
 Account_Name = input("Account Name: ")
-# requests_cache.install_cache('demo_cache')
+
+# API to call Cycle Time/Lead Time
 Speed_API = "https://{}.leankit.com/io/reporting/speed".format(Account_Name)
 
 # Input from user for authentication
@@ -21,20 +26,34 @@ try:
 except Exception as error:
     print('ERROR', error)
 
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijg1MzE5ODc3NCIsIm5hbWUiOiJBZGFtLlNhZGlxQHJicy5jb20iLCJlbWFpbCI6IkFkYW0uU2FkaXFAcmJzLmNvbSIsIm9yZ0lkIjoiNTMyOTE0NDgyIiwiY2FjaGVFeHBpcmF0aW9uIjoxNDQwLCJleHAiOjE1NjQ2NjAzNTksImlzcyI6ImxlYW5raXQtcmVwb3J0aW5nLWFwaSIsImF1ZCI6ImxlYW5raXQtcmVwb3J0aW5nLWFwaSIsImlhdCI6MTU2NDA1NTU1OX0.fBc-tboIMcAniXSS0nx7UHauQdhTGr9P9nUmW4oog60"
-
 req = requests.sessions.session()
 
+# token for access to leankit
+body = {
+	"email": email,
+	"accountName": Account_Name,
+	"password": password
+}
+headers = {'content-type': 'application/json', 'Cache-Control': 'no-cache', "Pragma": "no-cache"}
+reply = req.post("https://{}.leankit.com/io/reporting/auth".format(Account_Name), data=json.dumps(
+            body), headers=headers)
+content = reply._content
+content_json = json.loads(content.decode('utf8'))
+token = content_json["token"]
+
+# Decode UTF-8 bytes to Unicode
+content_json = content.decode('utf8')
+
+# Get all cards from LeanKit to compare
 card_response = req.get("https://{}.leankit.com/io/reporting/export/cards?token={}".format(Account_Name,
         token), auth=HTTPBasicAuth(email, password), headers={'Cache-Control': 'no-cache', "Pragma": "no-cache"})
-
 cardsInfo = card_response._content.decode('utf8')
 
 # save cardsInfo as out.csv
 with open('out.csv', 'w') as f:
     print(cardsInfo, file=f)
 
-## remove empty lines from file
+# remove empty lines from file
 readFile = open("out.csv")
 lines = readFile.readlines()
 readFile.close()
@@ -42,48 +61,36 @@ w = open("out.csv", 'w')
 w.writelines([item for item in lines[:-2]])
 w.close()
 
-check = [686342791, 709356071]
-
 # function to find the start lanes, returning them as a list
 def find_lead_time(cards):
 
     cardsFound = 0
     newCards = json.loads(cards)
 
-    total_years = 0
-    total_months = 0
-    total_days = 0
-
     total_time = 0
     lead_time = 0
-    
+     
     # # line[0] = CardID
     # # line[15] = Creation date
     # # line[19] = Finish date
 
-    # #pragma ompd
     with open('out.csv', 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
+        # Go through the saved cards
         for line in csv_reader:
+        	# Go through the cards returned from Speed API
             for card in newCards:
+            	# If the card has been found
                 if (line[0] == card['cardId']):
+                	# Some cards don't have a finish date or start date for no apparent reason
                     if (not((line[19][:4] == '') or line[15][:4] == '')):
                         cardsFound += 1
-                        print(line)
-                        print(line[19])
-                        print(line[15])
-                        years_diff = float(line[19][:4]) - float(line[15][:4])
-                        months_diff = float(line[19][5:7]) - float(line[15][5:7])
-                        days_diff = float(line[19][9:10]) - float(line[15][8:10])
 
+                        # Finding start and end dates from string
                         start_year, start_month, start_day = (
                             float(line[15][:4]), float(line[15][5:7]), float(line[15][8:10]))
                         end_year, end_month, end_day = (
                             float(line[19][:4]), float(line[19][5:7]), float(line[19][8:10]))
-
-                        total_years += years_diff
-                        total_months += months_diff
-                        total_days += days_diff
 
                         # Converting difference to days
                         f_date = date(int(start_year), int(start_month), int(start_day))
@@ -91,9 +98,10 @@ def find_lead_time(cards):
                         delta = l_date - f_date
                         total_time += float(delta.days)
 
+        # If cards have been found
         if (not(cardsFound == 0)):
+        	# calculate lead time
             lead_time = int(total_time)/int(cardsFound)
-            print(total_time, cardsFound)
             print("Leadtime in days:", lead_time)
             print("")
             return lead_time
@@ -110,9 +118,9 @@ response = req.get("https://{}.leankit.com/kanban/api/boards".format(
     Account_Name), auth=HTTPBasicAuth(email, password), headers={'Cache-Control': 'no-cache', "Pragma": "no-cache"})
 feedback = response._content.decode('utf8')
 replyData = json.loads(feedback)["ReplyData"][0]
-boardIds = []
 
 # Loop through all boards and retrieve Id
+boardIds = []
 for board in replyData:
     boardIds.append(board["Id"])
 
@@ -123,7 +131,7 @@ lead_times = []
 # Go through each boardId and find lead time
 for i in boardIds:
     req.cookies.clear()
-    # Retrieve board information
+    # Retrieve board information per boardID
     response = req.get("https://{}.leankit.com/kanban/api/board/{}/GetBoardIdentifiers".format(Account_Name, i), auth=HTTPBasicAuth(email, password), headers={'Cache-Control': 'no-cache', "Pragma": "no-cache"})
     feedback = json.loads(response._content.decode('utf8'))
     replyData = feedback['ReplyData'][0]
@@ -143,7 +151,7 @@ for i in boardIds:
             "finishLanes": end_lanes,
             "startDate": startDate,
             "endDate": endDate,
-            "timeOffset": -60
+            "timeOffset": -60 # Always used in LeanKit Speed API call
         }
 
         headers = {'content-type': 'application/json', 'Cache-Control': 'no-cache', "Pragma": "no-cache"}
@@ -154,7 +162,7 @@ for i in boardIds:
     
         # Decode UTF-8 bytes to Unicode
         content_json = content.decode('utf8')
-        if (response.status_code == 200):
+        if (response.status_code == 200): #If the Speed API call was successful
             # Find lead time and save results to array
             lead_times.append(
                 {
